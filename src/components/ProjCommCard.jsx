f2ba6d8a4bf6debf5bb8cbb3457a58268544461a
Aditya Sharma <adityashf7@gmail.com>
2023-01-08 01:33:51 +0530

    Update Communities.jsx

--- src/components/ProjCommCard.jsx
+++ src/components/ProjCommCard.jsx
@@ -9,6 +9,14 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const ProjCommCard = ({ data, type }) => {
 
+    const Modal = ({ children }) => (
+        <div className="modal">
+          <div className="modal-content">
+            {children}
+          </div>
+        </div>
+      );
+
 
     const { getProjects, getCommunities } = useContext(TekContext);
 
@@ -34,91 +42,108 @@ const ProjCommCard = ({ data, type }) => {
     }
 
     return (
+        // <>
+
+        //     <ToastContainer
+        //         position="top-right"
+        //         autoClose={1000}
+        //         hideProgressBar={false}
+        //         newestOnTop={false}
+        //         closeOnClick
+        //         rtl={false}
+        //         pauseOnFocusLoss
+        //         draggable
+        //         pauseOnHover
+        //         closeButton={false}
+        //         limit={1}
+        //     />
+
+
+        //     <div className="commcard_main_parent">
+        //         <div className={type === "project" ? "commcard_card" : "commcard_card2"}>
+
+
+        //             <div className="commcard_card_imgdiv">
+        //                 <img src={data.pic} alt="" />
+        //             </div>
+
+
+        //             <div className={type === "project" ? "commcard_card_textdiv" : "commcard_card_textdiv2"}>
+        //                 <h3>{data.name}</h3>
+
+        //                 {type === "project" && <div className="commcard_usernamediv">
+        //                     <img src="https://i.ibb.co/5BSd278/Userimg.png" className="commcard_userimg" alt="" />
+        //                     <p className="commcard_username">{data.user_name}</p>
+        //                 </div>}
+
+
+        //                 <p className="commcard_desc">{data.desc}</p>
+        //                 {/* 190 max */}
+
+        //                 <div className="commcard_card_textdiv_socials">
+
+        //                     {type === "project" ? <div>
+        //                         {data.gh_link && <BsGithub className="card_socials card_socials_github" onClick={() => {
+        //                             window.open(data.gh_link);
+        //                         }} />}
+
+        //                         {data.yt_link && <BsYoutube className="card_socials card_socials_youtube" />}
+
+        //                         {
+        //                             window.location.pathname.includes("editprofile") && (<>
+        //                                 <Del className="pf_techskills_del" style={{ height: "20px" }}
+        //                                     onClick={() => { handleDelete(data._id) }} />
+        //                             </>)
+        //                         }
+
+        //                     </div>
+        //                         :
+        //                         <div style={{ display: "flex", justifyContent: "space-between", }}>
+
+        //                             <div>
+        //                                 <BsYoutube className="card_socials card_socials_youtube" />
+        //                                 <BsGithub className="card_socials card_socials_github" />
+        //                                 <BsDiscord className="card_socials card_socials_discord" />
+        //                                 <BsTwitter className="card_socials card_socials_twitter" />
+        //                             </div>
+
+        //                             {
+        //                                 window.location.pathname.includes("editprofile") && (<>
+        //                                     <Del className="pf_techskills_del" style={{ height: "20px", }} onClick={() => { handleDelete(data._id) }} />
+        //                                 </>)
+        //                             }
+
+
+        //                         </div>
+
+        //                     }
+
+
+
+
+
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </>
         <>
-
-            <ToastContainer
-                position="top-right"
-                autoClose={1000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                closeButton={false}
-                limit={1}
-            />
-
-
-            <div className="commcard_main_parent">
-                <div className={type === "project" ? "commcard_card" : "commcard_card2"}>
-
-
-                    <div className="commcard_card_imgdiv">
-                        <img src={data.pic} alt="" />
-                    </div>
-
-
-                    <div className={type === "project" ? "commcard_card_textdiv" : "commcard_card_textdiv2"}>
-                        <h3>{data.name}</h3>
-
-                        {type === "project" && <div className="commcard_usernamediv">
-                            <img src="https://i.ibb.co/5BSd278/Userimg.png" className="commcard_userimg" alt="" />
-                            <p className="commcard_username">{data.user_name}</p>
-                        </div>}
-
-
-                        <p className="commcard_desc">{data.desc}</p>
-                        {/* 190 max */}
-
-                        <div className="commcard_card_textdiv_socials">
-
-                            {type === "project" ? <div>
-                                {data.gh_link && <BsGithub className="card_socials card_socials_github" onClick={() => {
-                                    window.open(data.gh_link);
-                                }} />}
-
-                                {data.yt_link && <BsYoutube className="card_socials card_socials_youtube" />}
-
-                                {
-                                    window.location.pathname.includes("editprofile") && (<>
-                                        <Del className="pf_techskills_del" style={{ height: "20px" }}
-                                            onClick={() => { handleDelete(data._id) }} />
-                                    </>)
-                                }
-
-                            </div>
-                                :
-                                <div style={{ display: "flex", justifyContent: "space-between", }}>
-
-                                    <div>
-                                        <BsYoutube className="card_socials card_socials_youtube" />
-                                        <BsGithub className="card_socials card_socials_github" />
-                                        <BsDiscord className="card_socials card_socials_discord" />
-                                        <BsTwitter className="card_socials card_socials_twitter" />
-                                    </div>
-
-                                    {
-                                        window.location.pathname.includes("editprofile") && (<>
-                                            <Del className="pf_techskills_del" style={{ height: "20px", }} onClick={() => { handleDelete(data._id) }} />
-                                        </>)
-                                    }
-
-
-                                </div>
-
-                            }
-
-
-
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <div className="commcard_main_parent">
+      <div className="commcard_card2">
+        <div className="commcard_card_imgdiv">
+          <img src={data.pic} alt="" />
+        </div>
+        <div className="commcard_card_textdiv2">
+          <h3>{data.name}</h3>
+          <ul>
+            {data.details.map(detail => (
+              <li key={detail.id}><a href={detail.link}>{detail.text}</a></li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+      </>
     );
 };
 
--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -1,60 +1,59 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import ProjCommCard from "../components/ProjCommCard";
-import { getallcommunities } from "../service/DataApi";
+import React from "react";
 import "../styles/Communities.css";
 
 const Communities = () => {
-    const [communities, setCommunities] = useState([]);
+  return (
+    <div className="comm_main_parent container">
+      <div className="comm_subparent">
+        <div className="comm_header">
+          <p className="comm_header_p1">Join Top Tech Communities Around The Globe</p>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <p className="comm_header_p2"></p>
+          </div>
+        </div>
 
-    const getAllcommunities = async () => {
-        const response = await getallcommunities()
-
-        if (response.status === 201) {
-            setCommunities(response.data)
-            console.log(response.data)
-        }
-    }
-
-
-    useEffect(() => {
-        getAllcommunities()
-    }, []);
-
-
-    return (
-        <>
-            <div className="comm_main_parent container">
-                <div className="comm_subparent">
-                    <div className="comm_header">
-
-                        <p className="comm_header_p1">
-                        Join Top Tech Communities Around The Globe
-                            {" "}
-                        </p>
-
-                        <div style={{ display: "flex", justifyContent: "center" }}>
-                            <p className="comm_header_p2">
-                            
-                            </p>
-                        </div>
-
-
-                    </div>
-
-
-
-                    <div className="comm_carddiv">
-                        {communities.map((data) => (
-                            <div className="user">
-                                <ProjCommCard data={data} />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+        <ul className="comm_list">
+          <li className="comm_list_item">
+            <h2 className="comm_name">EddieHub</h2>
+            <p className="comm_description">An Open Source community aimed at encouraging and promoting communication and technical expertise in an inclusive and welcoming environment</p>
+            <a href="https://github.com/EddieHubCommunity" className="comm_link">Join now</a>
+            {/* <a href="#"><i className="fab fa-github"></i></a>
+            <a href="#"><i className="fab fa-twitter"></i></a> */}
+          </li>
+          <li className="comm_list_item">
+            <h2 className="comm_name">WeMakeDevs</h2>
+            <p className="comm_description">An Open Source community focused to educate everyone, irrespective of their college or branch, technical or non-technical background</p>
+            <a href="https://github.com/WeMakeDevs" className="comm_link">Join now</a>
+          </li>
+          <li className="comm_list_item">
+            <h2 className="comm_name">Acccessible For All</h2>
+            <p className="comm_description">A community dedicated to making the web more accessible for everyone, and that starts by educating designers and developers in accessibility best practices from an early stage</p>
+            <a href="https://github.com/AccessibleForAll" className="comm_link">Join now</a>
+          </li>
+          <li className="comm_list_item">
+            <h2 className="comm_name">Hack Club</h2>
+            <p className="comm_description">A worldwide community of high school hackers. By the students, for the students.</p>
+            <a href="https://github.com/hackclub" className="comm_link">Join now</a>
+          </li>
+          <li className="comm_list_item">
+            <h2 className="comm_name">Cloud Community</h2>
+            <p className="comm_description">Cloud Study Network is a global community of 8000+ enthusiasts interested in cloud native & devops technologies.</p>
+            <a href="https://github.com/cloudcommunity" className="comm_link">Join now</a>
+          </li>
+          <li className="comm_list_item">
+            <h2 className="comm_name">CodeVisors</h2>
+            <p className="comm_description">A community driven by tech enthusiasts and open source contributors to help young students to improve there technical skills by providing webinars.</p>
+            <a href="https://github.com/CodeVisors" className="comm_link">Join now</a>
+          </li>
+          <li className="comm_list_item">
+            <h2 className="comm_name">FossAsia</h2>
+            <p className="comm_description">Open Technologies developed in Asia and Around the Globe</p>
+            <a href="https://github.com/fossasia" className="comm_link">Join now</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Communities;
