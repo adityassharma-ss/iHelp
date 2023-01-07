f71b7e05f399f381dd204b1231e342d1f7c300a1
Aditya Sharma <adityashf7@gmail.com>
2023-01-08 03:10:11 +0530

    Mentorship updated

--- src/pages/Mentorships.jsx
+++ src/pages/Mentorships.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Contacts from '../styles/Contacts.css'
+import Frontend from './roadmaps/Frontend';
 
 const Mentorships = () => {
   const [selectedSubject, setSelectedSubject] = useState('subject1') // state to keep track of the selected subject
@@ -12,7 +13,7 @@ const Mentorships = () => {
         <div className='navbar-item' onClick={() => setSelectedSubject('subject1')}>
           Full Stack Development
         </div>
-        <div className='navbar-item' onClick={() => setSelectedSubject('subject2')}>
+        <div className='navbar-item' onClick={() => setSelectedSubject('subject2' )}>
           Frontend Development
         </div>
         <div className='navbar-item' onClick={() => setSelectedSubject('subject3')}>
@@ -55,12 +56,12 @@ const Mentorships = () => {
       <div className='roadmap-container'>
         {selectedSubject === 'subject1' && (
           <div className='roadmap-item'>
-            {/* subject 1 course roadmap */}
+            {}
           </div>
         )}
         {selectedSubject === 'subject2' && (
           <div className='roadmap-item'>
-            {   /* subject 2 course roadmap */ }
+            { <Frontend /> }
           </div>
         )}
         {selectedSubject === 'subject3' && (
