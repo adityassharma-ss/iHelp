740e83f5890f2a3e4123e1510aea6fabbe7e43e1
Aditya Sharma <adityashf7@gmail.com>
2023-01-09 10:01:06 +0530

    Updated Frontend Roadmap

--- src/pages/roadmaps/Frontend.jsx
+++ src/pages/roadmaps/Frontend.jsx
@@ -4,12 +4,33 @@ import frontend from '../mapstyles/frontend.css'
 const Frontend = () => {
   return (
     <div className='front-main'>
-      <div className='front-sub'>Frontend</div>
+      <div className='front-sub'>Frontend RoadMap</div>
       <ul>
         <li><a href="https://frontendmasters.com/guides/front-end-handbook/2018/what-is-a-FD.html">What is a frontend developer? - frontendmasters.com</a></li>
         <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Front-end_web_developer">Frontend web developer - Mozilla Developer docs</a></li>
         <li><a href="https://www.freecodecamp.org/news/front-end-developer-what-is-front-end-development-explained-in-plain-english/">What is frontend development - Freecodecamp</a></li>
-        ...
+        <li><a href="https://www.youtube.com/watch?v=qyHyFsT7Hig">Frontend Development explained - Mayuko(Youtube)</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/Front-end_web_development">Frontend web development - Wikipedia</a></li>
+        <li><a href="https://www.youtube.com/watch?v=kUMe1FH4CHE">Learn HTML - FreeCodeCamp(Youtube)</a></li>
+        <li><a href="https://www.codecademy.com/learn/learn-html">Learn HTML - Codecadmeny</a></li>
+        <li><a href="https://www.w3schools.com/html/">HTML Tutorial - W3schools</a></li>
+        <li><a href="https://www.tutorialspoint.com/html/index.htm">HTML Tutorial - TutorialsPoint</a></li>
+        <li><a href="https://www.youtube.com/watch?v=pQN-pnXPaVg">HTML course, build a website - FreeCodeCamp(Youtube)</a></li>
+        <li><a href="https://web.dev/learn/css/">Learn CSS - web.dev</a></li>
+        <li><a href="https://www.codecademy.com/learn/learn-css">Learn CSS - codecademy</a></li>
+        <li><a href="https://www.youtube.com/watch?v=PkZNo7MFNFg">Learn JavaScript - FreeCodeCamp(Youtube)</a></li>
+        <li><a href="https://www.youtube.com/watch?v=W6NZfCO5SIk">JavaScript Tutorial - Programming with Mosh(Youtube)</a></li>
+        <li><a href="https://www.youtube.com/watch?v=jS4aFq5-91M">JavaScript Programming - FreeCodeCamp(Youtube)</a></li>
+        <li><a href="https://www.w3schools.com/js/">JavaScript Tutorial - w3schools</a></li>
+        <li><a href="https://www.codecademy.com/learn/introduction-to-javascript">Learn JavaScript - Codecademy</a></li>
+        <li><a href="https://www.freecodecamp.org/news/learn-javascript-free-js-courses-for-beginners/">Free JS Courses for Beginners - FreeCodeCamp</a></li>
+        <div className='front-sub'>FrameWorks/Libraries</div>
+        <li><a href="https://www.youtube.com/watch?v=bMknfKXIFA8">React Tutorial</a></li>
+        <li><a href="https://www.youtube.com/watch?v=2OHbjep_WjQ">Angular Tutorial</a></li>
+        <li><a href="https://www.browserstack.com/guide/top-css-frameworks">CSS-Frameworks</a></li>
+        <div className='front-sub'>Frontend-Projects</div>
+        <li><a href="https://www.frontendmentor.io/">Frontend-Mentor.io</a></li>
+        <li><a href="https://www.frontendpractice.com/">Frontend-Practice</a></li>
       </ul>
     </div>
   )
