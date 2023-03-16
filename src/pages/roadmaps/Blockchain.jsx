6de997e3c0c23d85ae1433dac4d27b4337c3da7d
Aditya Sharma <adityashf7@gmail.com>
2023-03-16 22:51:47 +0530

    updated mentorship page

---
+++ src/pages/roadmaps/Blockchain.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import '../mapstyles/blockchain.css'
+
+const Blockchain = () => {
+  return (
+    <div className='block-main'>
+      <div className='block-sub'>Blockchain RoadMap</div>
+      <ul>
+      {/* <div className='block-sub'>Blockchain Fundamentals</div> */}
+        <li><a href="https://www.freecodecamp.org/news/what-is-web3/">What is Web3?</a></li>
+        <li><a href="https://www.geeksforgeeks.org/blockchain-technology-introduction/">What is Blockchain?</a></li>
+        <li><a href="https://www.oswego.edu/cts/basics-about-cryptocurrency">Crypto-Basics</a></li>
+        <li><a href="https://www.simplilearn.com/what-is-web-1-0-web-2-0-and-web-3-0-with-their-difference-article">Difference between Web1-Web2-Web3</a></li>
+        <li><a href="https://ethereum.org/en/developers/docs/smart-contracts/">Smart Contracts</a></li>
+        <li><a href="https://www.businessinsider.com/personal-finance/crypto-wallet?IR=T">Crypro-Wallet</a></li>
+        <li><a href="https://www.gemini.com/cryptopedia/blockchain-layer-2-network-layer-1-network">Blockchain: Layer1 Vs Layer2</a></li>
+        <li><a href="https://chain.link/education/blockchain-oracles">Blockchain-Oracle</a></li>
+        <li><a href="https://www.forbes.com/advisor/investing/cryptocurrency/nft-non-fungible-token/">NFTs</a></li>
+        <li><a href="https://www.forbes.com/sites/cathyhackl/2021/06/01/what-are-daos-and-why-you-should-pay-attention/?sh=343b04067305">DAOs</a></li>
+        <li><a href="https://ethereum.org/en/developers/docs/dapps/">Introduction To dApps</a></li>
+        <li><a href="https://ethereum.org/en/developers/docs/dapps/">EVM & Non-EVM chains</a></li>
+        <li><a href="https://www.web3.university/tracks/road-to-web3">Web3 University-Road To Web3</a></li>
+        <div className='block-sub'>Choose A Blockchain</div>
+        <li><a href="https://ethereum.org/en/">Ethereum</a></li>
+        <li><a href="https://solana.com/">Solana</a></li>
+        <div className='block-sub'>Ethereum Concepts</div>
+        <li><a href="https://ethereum.org/en/developers/docs/accounts/">Accounts</a></li>
+        <li><a href="https://ethereum.org/en/developers/docs/transactions/">Transactions</a></li>
+        <li><a href="https://ethereum.org/en/developers/docs/evm/">Ethereum Virtual Machines</a></li>
+        <div className='block-sub'>Choosing a Language</div>
+        <li><a href="https://docs.soliditylang.org/en/v0.8.19/">Solidity</a></li>
+        <li><a href="https://docs.vyperlang.org/en/stable/">Vyper</a></li>
+        <li><a href="https://docs.solana.com/developing/on-chain-programs/developing-rust">Rust(Solana)</a></li>
+        <div className='block-sub'>Blockchain-Projects</div>
+        <li><a href="https://dev.to/edge-and-node/the-complete-guide-to-full-stack-web3-development-4g74">Build a Fullstack WEB3 project</a></li>
+        <li><a href="https://www.theinsaneapp.com/2022/05/best-web3-projects.html">80+ Web3 Dapps Projects</a></li>
+        <li><a href="https://www.pointer.gg/tutorials">Build a Web3 Project</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Blockchain
