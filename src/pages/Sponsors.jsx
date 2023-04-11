44c90b500f8e104526b7965bc67a77084875d6b5
Aditya Sharma <adityashf7@gmail.com>
2023-04-11 23:56:37 +0530

    updated sponsors page

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,12 +1,96 @@
-import React from 'react'
-import Events from '../styles/Sponsors.css'
+// import React from 'react'
+// import Events from '../styles/Sponsors.css'
+
+// const Sponsor = () => {
+//   return (
+//     <div className='sponsor-parent'>
+//         <div className='sponsor-child'>
+//             <h1 className='sponsor-child-h1'> Coming Soon...</h1>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Sponsor;
+
+// import React from 'react';
+// import Events from '../styles/Sponsors.css';
+
+// const Sponsor = () => {
+//   return (
+//     <div className='sponsor-parent'>
+//         <div className='sponsor-child'>
+//             <h1 className='sponsor-child-h1'>Our Sponsors</h1>
+//             <p className='sponsor-child-p'>We are grateful for the support of the following companies</p>
+//             <div className='sponsor-list'>
+//                 <div className='sponsor-item'>
+//                     <a href='https://example.com'>
+//                         <img src='https://example.com/logo.png' alt='Example Corp.' />
+//                     </a>
+//                 </div>
+//                 <div className='sponsor-item'>
+//                     <a href='https://example.com'>
+//                         <img src='https://example.com/logo.png' alt='Example Corp.' />
+//                     </a>
+//                 </div>
+//                 <div className='sponsor-item'>
+//                     <a href='https://example.com'>
+//                         <img src='https://example.com/logo.png' alt='Example Corp.' />
+//                     </a>
+//                 </div>
+//                 <div className='sponsor-item'>
+//                     <a href='https://example.com'>
+//                         <img src='https://example.com/logo.png' alt='Example Corp.' />
+//                     </a>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Sponsor;
+
+import React from 'react';
+import Events from '../styles/Sponsors.css';
 
 const Sponsor = () => {
   return (
     <div className='sponsor-parent'>
         <div className='sponsor-child'>
-            <h1 className='sponsor-child-h1'> Coming Soon...</h1>
+            <h1 className='sponsor-child-h1'>Our Sponsors</h1>
+            <p className='sponsor-child-p'>We are grateful for the support of the following companies</p>
+            <div className='sponsor-list'>
+                <div className='sponsor-item'>
+                    <a href='https://example.com'>
+                        <img src='https://example.com/logo.png' alt='Coming Soon..' />
+                    </a>
+                </div>
+                <div className='sponsor-item'>
+                    <a href='https://example.com'>
+                        <img src='https://example.com/logo.png' alt='Coming Soon..' />
+                    </a>
+                </div>
+                <div className='sponsor-item'>
+                    <a href='https://example.com'>
+                        <img src='https://example.com/logo.png' alt='Coming Soon..' />
+                    </a>
+                </div>
+                <div className='sponsor-item'>
+                    <a href='https://example.com'>
+                        <img src='https://example.com/logo.png' alt='Coming Soon..' />
+                    </a>
+                </div>
+            </div>
+            <div className='sponsor-callout'>
+                <p>Interested in becoming a sponsor?</p>
+                <button className='sponsor-button'>Contact Us</button>
+            </div>
+            <div className='sponsor-footer'>
+                <p>&copy; 2023 iHelp</p>
+            </div>
         </div>
+        
     </div>
   )
 }
